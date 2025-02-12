6550e5c4ad3b2f78db60664ae5084bb6fec30663
nourhan <nourhansaeed707@gmail.com>
2025-02-12 23:48:36 +0200

    add security config in product service and order service:

--- product-service/src/main/java/com/example/product_service/client/UserClient.java
+++ product-service/src/main/java/com/example/product_service/client/UserClient.java
@@ -1,8 +1,10 @@
 package com.example.product_service.client;
 import com.example.product_service.DTO.Users;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import java.util.Optional;
 
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface UserClient {
     @GetMapping("/")
     ResponseEntity<Optional<Users>> getUserInfo(@RequestHeader("Authorization") String token);
+
+    @GetMapping("/get-by-email/{email}")
+    Users getUserByEmail(@PathVariable("email") String email, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
+
 }
---
+++ product-service/src/main/java/com/example/product_service/jwt/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.example.product_service.jwt;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import io.jsonwebtoken.io.Decoders;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    public static final String SECRET = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
